9db6c06b66fe1bb53e51cf46387c6cfc0de66e24
Seppedc <decraeneseppe@hotmail.be>
2023-05-13 19:18:14 +0200

    Remove the code from workshop

--- Workshop/Workshop Files/main.js
+++ Workshop/Workshop Files/main.js
@@ -3,51 +3,45 @@ import * as CANNON from 'cannon-es';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import CannonDebugger from 'cannon-es-debugger';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-//setup
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100000 );
-scene.background = new THREE.Color("white");
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+//ToDo Phase 1
+
+
+//extra controls for canvas and visual helpers
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 const axesHelper = new THREE.AxesHelper(8);
 scene.add(axesHelper);
-
-
-camera.position.x = 17;
-camera.position.y = 11;
-camera.position.z = 0;
-
+//First object 
 const geometry = new THREE.SphereGeometry(1);
 const material = new THREE.MeshNormalMaterial();
 const sphereMesh = new THREE.Mesh(geometry, material);
 scene.add(sphereMesh);
 
-const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
-const boxMaterial = new THREE.MeshNormalMaterial();
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(boxMesh);
 
-//vloer
-let tex = new THREE.TextureLoader().load("https://upload.wikimedia.org/wikipedia/commons/4/4c/Grass_Texture.png")
-tex.anisotropy = 32
-tex.repeat.set(100, 100)
-tex.wrapT = THREE.RepeatWrapping
-tex.wrapS = THREE.RepeatWrapping
+//ToDo Phase 2 Part 1 
+
+
+let texture = null;
+
+//extra params over de vloer
+texture.anisotropy = 32
+texture.repeat.set(100, 100)
+texture.wrapT = THREE.RepeatWrapping
+texture.wrapS = THREE.RepeatWrapping
 let geo = new THREE.PlaneBufferGeometry(100, 100)
 let mat = new THREE.MeshLambertMaterial({
-  map: tex
+  map: texture
 })
 let mesh = new THREE.Mesh(geo, mat)
 mesh.position.set(0, -5, 0)
 mesh.rotation.set(Math.PI / -2, 0, 0)
 scene.add(mesh)
-//lichtbron toevoegen 
-const sun = new THREE.DirectionalLight(0xffffcc)
-sun.position.set(0, 1, 0)
-scene.add(sun)
+
+
+//ToDo Phase 2 part 2 
+
+
 
 
 //physics wereld aanmaken 
@@ -66,7 +60,7 @@ const cannonDebugger = new CannonDebugger(scene, physicsWorld, {
     color: 0xff0000,
 });
 
-//physieke objecten maken 
+//eerste physieke object maken 
 const sphereBody = new CANNON.Body({
     mass: 5,
     shape: new CANNON.Sphere(1),
@@ -74,54 +68,27 @@ const sphereBody = new CANNON.Body({
 sphereBody.position.set(0, 7, 0);
 physicsWorld.addBody(sphereBody);
 
-const boxBody = new CANNON.Body({
-    mass: 5,
-    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
-});
-boxBody.position.set(1, 10, 0);
-physicsWorld.addBody(boxBody);
+//ToDo Phase 3
 
-//3dmodel 
+
+//3dmodel loader
 const loader = new GLTFLoader();
 
-loader.load(
-	'/assets/bowling_pin/pin.gltf',
-	function ( gltf ) {
+//ToDo Phase  5
 
-        gltf.scene.scale.set(10,10,10);
-		scene.add( gltf.scene );
-	},
-	function ( xhr ) {
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-	},
-	function ( error ) {
+//ToDo Phase  6
+ 
 
-		console.log( 'An error happened' );
-
-	}
-);
-
-//car 
-
-//controls
-
-//tower
-
+//ToDo Phase  7
 
 
 const animate = () => {
-    physicsWorld.fixedStep();
-    cannonDebugger.update();
-    mesh.position.copy(groundBody.position);
-    mesh.quaternion.copy(groundBody.quaternion);
-    boxMesh.position.copy(boxBody.position);
-    boxMesh.quaternion.copy(boxBody.quaternion);
-    sphereMesh.position.copy(sphereBody.position);
-    sphereMesh.quaternion.copy(sphereBody.quaternion);
+    //ToDo Phase  4
+    
     requestAnimationFrame(animate);
     controls.update();
-    renderer.render( scene, camera );
+    //ToDo Phase 1 (moet als laatste aners werken sommige andere onderdelen niet)
 };
 animate();
